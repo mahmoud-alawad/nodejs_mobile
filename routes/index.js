33c1c797e181a5f5e777afe78301d26f858aa117
mahmoud <awad25.ma@gmail.com>
2023-01-05 23:30:13 +0100

    some fixes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 //routes
 const express = require('express')
 const router = express.Router();
-const AuthenticationController = require('./auth');
 
-router.route('/').get((req,res)=>  res.status(200).render('home', { routeName: req.route.path.replace('/', ' ') }));
-router.route('/login').get( AuthenticationController.view).post( AuthenticationController.login);
+router.route('/').get((req,res)=>  res.status(200).render('home', { title: req.route.path.replace('/', ' ') }));
 
 module.exports = router;
